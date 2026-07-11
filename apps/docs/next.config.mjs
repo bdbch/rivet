296236f2cf9a5a400b17c6bcd626f7ab60435a2f
bdbch <dominik@bdbch.com>
2026-07-11 02:46:48 +0200

    add basepath

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,10 +1,12 @@
 import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
+const basePath = process.env.GITHUB_ACTIONS === 'true' ? '/rivet' : undefined;
 
 /** @type {import('next').NextConfig} */
 const config = {
   output: 'export',
+  basePath,
   reactStrictMode: true,
 };
 
